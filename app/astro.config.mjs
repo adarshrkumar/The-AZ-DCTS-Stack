870b9d2843c26fd1152185fe507ce87cd2570ee0
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-01-02 19:50:03 -0800

    u

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import clerk from '@clerk/astro';
 import { VitePWA } from 'vite-plugin-pwa';
 
